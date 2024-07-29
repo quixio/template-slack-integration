f78f8eafa68d414b6313bf06d5d1f16a409d5acf
Steve Rosam <steve+quix-org@quix.io>
2024-07-29 16:42:04 +0000

    Edited 'main.py'

--- Create Embeddings/main.py
+++ Create Embeddings/main.py
@@ -72,6 +72,8 @@ sdf["embeddings"] = sdf.apply(create_embeddings, stateful=False)
 # Update the timestamp column to the current time in nanoseconds
 sdf["Timestamp"] = sdf.apply(lambda row: time.time_ns())
 
+sdf["Timestamp"] = sdf.apply(lambda row: print(row))
+
 # Publish the processed SDF to a Kafka topic specified by the output_topic object.
 # sdf = sdf.to_topic(output_topic)
 
