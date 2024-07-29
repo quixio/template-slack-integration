8494c0b0296559353e2e56eecd596b18fb1d38f7
Steve Rosam <steve+quix-org@quix.io>
2024-07-29 14:05:26 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -8,7 +8,7 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
+@app.message("turtle-soup")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
