318fa79866ae6159aacc6c679fb43b47c2e7aa61
Steve Rosam <steve+quix-org@quix.io>
2024-07-29 15:47:22 +0000

    Edited 'main.py'

--- Message transformer/main.py
+++ Message transformer/main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v11", auto_offset_reset="earliest")
+app = Application(consumer_group="transformation-v12", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
