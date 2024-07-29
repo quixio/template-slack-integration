0b0e0bf4c1adc27e80e2d03f4af5156c48c70534
Steve Rosam <steve+quix-org@quix.io>
2024-07-29 15:10:16 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -2,6 +2,12 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from quixstreams import Application
+
+quix_app = Application()
+topic =  quix_app.topic(os.environ["output"])
+producer = quix_app.get_producer()
+
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
@@ -11,6 +17,7 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.message("")
 def message_hello(message, say):
     print(message)
+    producer.produce(topic.name, json.dumps(message), "messages")
 
 # Start your app
 if __name__ == "__main__":
