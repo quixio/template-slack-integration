851935ea1b9bff8f22c7247fd9f617a1dc406d4a
Steve Rosam <steve+quix-org@quix.io>
2024-07-29 14:52:06 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -8,27 +8,9 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("#sarcasm")
+@app.message("")
 def message_hello(message, say):
-
-    try:
-        # Delete the message
-        app.client.chat_delete(
-            channel=message['channel'],
-            ts=message['ts']
-        )
-        say("Message deleted!")
-    except Exception as e:
-        print(f"Error deleting message: {e}")
-        say("Failed to delete the message.")
-
-
-    test_str = str(message["text"][8:])
-
-    test_str = "geeksforgeeks"
-    print("The original string is: ",test_str)
-    res = "".join("".join(x) for x in zip(test_str[0::2].upper(), test_str[1::2].lower()))
-    print("The alternate case string is : " + res)
+    print(message)
 
 # Start your app
 if __name__ == "__main__":
