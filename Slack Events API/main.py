1571924878a93933bc87bd3d2a601ae807e2da9c
Steve Rosam <steve+quix-org@quix.io>
2024-09-04 17:10:59 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -10,16 +10,16 @@ topic =  quix_app.topic(os.environ["output"])
 producer = quix_app.get_producer()
 
 # Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 # Listens to incoming messages
-@app.message("")
+@slack_app.message("")
 def message_hello(message, say):
     print(message)
     producer.produce(topic.name, json.dumps(message), "messages")
 
 # handle message changed (someone edited their original message)
-@app.event("message")
+@slack_app.event("message")
 def handle_message_events(body, logger):
     producer.produce(topic.name, json.dumps(body), "messages")
 
@@ -27,4 +27,4 @@ def handle_message_events(body, logger):
 if __name__ == "__main__":
     # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
     tkn = 'xapp-1-A07DQ9Z6VUK-7700610932480-109d43bdfc2f2a638bf5fc845c382ac466bb18ab32f122e448a05258cfb6597f'
-    SocketModeHandler(app, tkn).start()
+    SocketModeHandler(slack_app, tkn).start()
