62f744c18710a049795b2d44f65fb800e6f2b0f1
Steve Rosam <steve+quix-org@quix.io>
2024-09-04 17:09:46 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -25,4 +25,6 @@ def handle_message_events(body, logger):
 
 # Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    tkn = 'xapp-1-A07DQ9Z6VUK-7700610932480-109d43bdfc2f2a638bf5fc845c382ac466bb18ab32f122e448a05258cfb6597f'
+    SocketModeHandler(app, tkn).start()
