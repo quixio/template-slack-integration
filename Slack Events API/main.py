a1604f2b4720fa06b0fb750af6ec438016783905
Steve Rosam <steve+quix-org@quix.io>
2024-09-06 09:45:48 +0000

    Edited 'main.py'

--- Slack Events API/main.py
+++ Slack Events API/main.py
@@ -4,7 +4,6 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from validate_email import validate_email
 
-
 from quixstreams import Application
 
 quix_app = Application()
@@ -19,16 +18,28 @@ slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @slack_app.command("/my-token")
 def handle_some_command(ack, body, logger, say):
 
+    msg = "Please try again and let us know your email address along with your request, so we can contact you if you win a prize."
+
+    def is_valid_email(email):
+        return validate_email(email)
+        
     def validate_json(input, field_name):
         if not input.get(field_name):
-            return f"Hi there! Please try again and let us know your email address along with your request, so we can contact you if you win a prize."      
+            return f"Hi there! {msg}"
     
     val_error = validate_json(body, 'text')
 
     if val_error != None:
         say(val_error)
     else:
-        say("Hi! Thanks for requesting an affiliate token. We have emailed it to you.")
+        email = input.get('text')
+        if is_valid_email(email):
+            print("Valid email address")
+        else:
+            print("Invalid email address")
+            say(f"Hi! Thanks for requesting an affiliate token. {msg}")
+
+            
         print(body)
         producer.produce(token_topic.name, json.dumps(body), "token_messages")
         
