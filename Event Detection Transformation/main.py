2de9ab370c5064c65cccc5bbc96c0f455ff13720
dev rel <devrel+infra-projects@quix.io>
2024-10-16 17:31:23 +0000

    Edited 'main.py'

--- Event Detection Transformation/main.py
+++ Event Detection Transformation/main.py
@@ -15,31 +15,31 @@ output_topic = app.topic(os.environ["output"])
 sdf = app.dataframe(input_topic)
 
 # Filter items out without brake value.
-sdf = sdf[sdf.contains("Brake")]
+# sdf = sdf[sdf.contains("Brake")]
 
-# Calculate hopping window of 1s with 200ms steps.
-sdf = sdf.apply(lambda row: row["Brake"]) \
-        .hopping_window(1000, 200).mean().final() 
+# # Calculate hopping window of 1s with 200ms steps.
+# sdf = sdf.apply(lambda row: row["Brake"]) \
+#         .hopping_window(1000, 200).mean().final() 
         
 sdf.print()
 
 # Filter only windows where average brake force exceeded 50%.
-sdf = sdf[sdf["value"] > 0.5]
+# sdf = sdf[sdf["value"] > 0.5]
 
 # Create nice JSON alert message.
-sdf = sdf.apply(lambda row: {
-    "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
-    "Alert": {
-        "Title": "Hard braking detected.",
-        "Message": "For last 1 second, average braking power was " + str(row["value"])
-    }
-})
+# sdf = sdf.apply(lambda row: {
+#     "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
+#     "Alert": {
+#         "Title": "Hard braking detected.",
+#         "Message": "For last 1 second, average braking power was " + str(row["value"])
+#     }
+# })
 
 # Print JSON messages in console.
 sdf.print()
 
 # Send the message to the output topic
-sdf.to_topic(output_topic)
+# sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
