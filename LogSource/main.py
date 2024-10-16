03c8a81993fafcecd01cdffc15826dca5ab2e5e1
SteveRosam <steve@quix.io>
2024-10-16 18:16:03 +0100

    log if log available

--- LogSource/main.py
+++ LogSource/main.py
@@ -1,11 +1,9 @@
-import requests
 from quixstreams import Application  # import the Quix Streams modules for interacting with Kafka:
 # (see https://quix.io/docs/quix-streams/v2-0-latest/api-reference/quixstreams.html for more details)
 
 # import additional modules as needed
-import random
 import os
-import json
+import requests
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
@@ -17,52 +15,46 @@ app = Application(consumer_group="data_source", auto_create_topics=True)  # crea
 topic_name = os.environ["output"]
 topic = app.topic(topic_name)
 
+def handle_message(deployment_id, message):
+    # create a pre-configured Producer object.
+    with app.get_producer() as producer:
+        # publish the data to the topic
+        producer.produce(
+            topic=topic.name,
+            key=deployment_id,
+            value=message,
+        )
+        print("All rows published")
 
-# this function loads the file and sends each row to the publisher
-def get_data():
-    import requests
-
-def stream_logs(deployment_id, base_url, headers=None):
+def stream_logs(deployment_id, base_url, message_handler, headers=None):
     url = f"{base_url}/deployments/{deployment_id}/logs/stream"
     
-    print("Deployment logs-----------------------------------------")
-    with requests.get(url, headers=headers, stream=True) as response:
-        response.raise_for_status()  # Check for HTTP errors
-        for line in response.iter_lines():
-            if line:
-                print(line.decode('utf-8'))
+    while True:
+        try:
+            print("Checking deployment logs availability...")
+            with requests.get(url, headers=headers, stream=True) as response:
+                response.raise_for_status()  # Check for HTTP errors
+                print("Deployment logs available. Streaming logs...")
+                for line in response.iter_lines():
+                    if line:
+                        print(line.decode('utf-8'))
+                        message_handler(deployment_id, {"code": 0, "message": line.decode('utf-8')})
+        except requests.exceptions.RequestException as e:
+            print(f"Error accessing logs: {e}")
+            message_handler(deployment_id, {"code": 1, "message": "Deployment is offline"})
+        except KeyboardInterrupt:
+            print("Exiting.")
+            break
 
 def main():
-
     # Example usage
-    deployment_id = "daff9f81-8d8a-4ae8-bb48-3676fb4e05e8"
+    deployment_id = os.environ["deployment_id"]
     base_url = "https://portal-api.platform.quix.io"
     headers = {
         "Authorization": "Bearer " + os.environ["token"]
     }
 
-    stream_logs(deployment_id, base_url, headers)
-
-
-    # # create a pre-configured Producer object.
-    # with app.get_producer() as producer:
-    #     # iterate over the data from the hardcoded dataset
-    #     data_with_id = get_data()
-    #     for row_data in data_with_id:
-
-    #         json_data = json.dumps(row_data)  # convert the row to JSON
-
-    #         # publish the data to the topic
-    #         producer.produce(
-    #             topic=topic.name,
-    #             key=row_data['host'],
-    #             value=json_data,
-    #         )
-
-    #         # for more help using QuixStreams see docs:
-    #         # https://quix.io/docs/quix-streams/introduction.html
-
-    #     print("All rows published")
+    stream_logs(deployment_id, base_url, handle_message, headers)
 
 
 if __name__ == "__main__":
