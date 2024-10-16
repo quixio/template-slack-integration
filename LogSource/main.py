a028050c42ffe8e2ecc72d1d8ab11c57d5187e2f
dev rel <devrel+infra-projects@quix.io>
2024-10-16 17:25:50 +0000

    Edited 'main.py'

--- LogSource/main.py
+++ LogSource/main.py
@@ -48,7 +48,7 @@ def stream_logs(deployment_id, base_url, message_handler, headers=None):
         except requests.exceptions.RequestException as e:
             print(f"Error accessing logs: {e}")
             message_handler(deployment_id, {"code": 1, "message": "Deployment is offline"})
-            time.
+            time.sleep(30)
         except KeyboardInterrupt:
             print("Exiting.")
             break
