b708ade5ae245afaaaf8adc873642e42ace2d86e
dev rel <devrel+infra-projects@quix.io>
2024-10-17 09:06:43 +0000

    Edited 'main.py'

--- Event Detection Transformation/main.py
+++ Event Detection Transformation/main.py
@@ -39,7 +39,7 @@ def find_message(main_string):
     print(main_string)
     j = json.loads(main_string)
     if "message" in j:
-        print(j)
+        print(j["message"])
     else:
         print("No Value")
 
